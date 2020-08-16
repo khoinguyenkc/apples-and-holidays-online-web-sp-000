0e3c12f8d7a34320940bd2edd7297407ca0d4730
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-16 08:57:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,5 +94,14 @@ all_supplies_in_holidays(testhash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbqlist = []
+  holiday_hash.each do | season, contentHS |
+    contentHS.each do | holiday, supplyarray |
+      if supplyarray.include?("BBQ")
+        bbqlist << holiday
+      end
+
+    end
+  end
 
 end

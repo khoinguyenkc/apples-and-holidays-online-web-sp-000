8d2a2deb589eaf41d107cfa835e450e86d22f8a8
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-16 08:27:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,8 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  # the arguments will bein the form like :fall as opposed to just fall.. so u dont have to do much There
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
